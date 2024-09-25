817d4083e816555765b1b7a3a182e8486dd0a3a1
Anton Bilous <anton.bilosus@gmail.com>
2024-09-25 12:02:54 +0300

    Register TaskDeleteView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TaskListView, TaskUpdateView
+from .views import TaskListView, TaskUpdateView, TaskDeleteView
 
 
 app_name = "tasks"
@@ -8,4 +8,5 @@ app_name = "tasks"
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
