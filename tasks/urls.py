3cb781e77115ba42e871498d4472687300155998
Anton Bilous <anton.bilosus@gmail.com>
2024-09-25 12:16:52 +0300

    Register task_change_status

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from .views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    task_change_status,
+)
 
 
 app_name = "tasks"
@@ -10,4 +16,9 @@ urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path(
+        "<int:pk>/change-status/",
+        task_change_status,
+        name="task-change-status",
+    ),
 ]
