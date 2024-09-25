9db77c0f11459bc9cb02455220a596984e3da2e0
Anton Bilous <anton.bilosus@gmail.com>
2024-09-25 11:47:52 +0300

    Create TaskUpdateView

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Task
 
@@ -6,3 +7,9 @@ from .models import Task
 class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.prefetch_related("tags")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
