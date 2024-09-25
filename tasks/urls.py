d4cc2c146643881a0a82caeae73bd02a0aa44fcb
Anton Bilous <anton.bilosus@gmail.com>
2024-09-25 11:48:07 +0300

    Register TaskUpdateView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TaskListView
+from .views import TaskListView, TaskUpdateView
 
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
