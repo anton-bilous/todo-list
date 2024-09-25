9c05446622ccbb07ef866e287245353002a19dda
Anton Bilous <anton.bilosus@gmail.com>
2024-09-25 12:02:32 +0300

    Create TaskDeleteView

--- tasks/views.py
+++ tasks/views.py
@@ -14,3 +14,8 @@ class TaskUpdateView(generic.UpdateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
